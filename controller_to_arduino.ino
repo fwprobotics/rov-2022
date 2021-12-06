c6f3299e886352fc2d2e98d5495ac1b924037662
bifft2 <bifft95@gmail.com>
2021-12-06 16:11:22 -0600

    Update controller_to_arduino.ino

--- controller_to_arduino.ino
+++ controller_to_arduino.ino
@@ -1,43 +1,43 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+#define THRUSTERS 4 // the number of Thrusters
+#define STOP 1500
 
-const int ledPin = 13; // the pin that the LED is attached to
-int incomingByte;      // a variable to read incoming serial data into
+int thrusterPins[THRUSTERS] = {2,3,4,5}; // Thrusters on pins 2 to 5
+Servo thrusters[THRUSTERS];
+
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-
-  // initialize serial communication:
-  Serial.begin(9600);
-  //myservo.write(90);
-  myservo.writeMicroseconds(1500);
+  Serial.begin(19200);
+  for(int i=0; i < THRUSTERS; i++)
+  { thrusters[i].attach(thrusterPins[i]); 
+  thrusters[i].writeMicroseconds(STOP);
+  }
   delay(7000);
-  // initialize the LED pin as an output:
-  // pinMode(ledPin, OUTPUT);
+  // put your setup code here, to run once:
+
 }
 
 void loop() {
-  // see if there's incoming serial data:
-  if (Serial.available() > 0) {
-    // read the oldest byte in the serial buffer:
-    incomingByte = Serial.read();
-    Serial.write(incomingByte);
-    // if it's a capital H (ASCII 72), turn on the LED:
-    if (incomingByte == 'N') {
-       //myservo.write(90); // sets the servo position according to the scaled value
-      //Serial.write('9');
-      myservo.writeMicroseconds(1500);
-    }
-     if (incomingByte == 'F') {
-       //myservo.write(115);
-       myservo.writeMicroseconds(1620);
-     }
-     if (incomingByte == 'B') {
-       //myservo.write(65); 
-       myservo.writeMicroseconds(1380);
-     }
-  }
+  serviceSerial();
   delay(15);
+  // put your main code here, to run repeatedly:
+
+}
+void serviceSerial()
+{
+  if (Serial.available())
+  { 
+    int speed = Serial.parseInt();
+    char ch = Serial.read();
+    if (ch >= 'a' && ch < 'a' + THRUSTERS)
+     { 
+        Serial.print("Thruster "); 
+        Serial.print(ch - 'a' + 1);
+        Serial.print("   set to ");
+        Serial.println(speed);
+       thrusters[ch - 'a'].write(speed);
+      }
+  }
 }
 
