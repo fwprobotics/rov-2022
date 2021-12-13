cb51c83e10f7ec2262ff05feccfc10bb180e4412
Suhani0617 <95658128+Suhani0617@users.noreply.github.com>
2021-12-13 17:04:26 -0600

    Update thruster_control.py
    updated code to add a second control and servo commands to control 1.

--- thruster_control.py
+++ thruster_control.py
@@ -1,7 +1,4 @@
-
-
 """
-
 """
 
 import pygame
@@ -19,7 +16,7 @@ joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_coun
 print(joysticks)
 
 # initialize serial monitor
-ser=serial.Serial('/dev/cu.usbmodem14201' , 19200, timeout=1)
+ser=serial.Serial('/dev/cu.usbmodem141301' , 19200, timeout=1)
 
 def writeToSerial(msg):
     print("writing", msg.encode())
@@ -33,77 +30,84 @@ while True:
     # Possible joystick actions: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
     # JOYBUTTONUP, JOYHATMOTION
     for event in pygame.event.get(): # User did something.
-        if event.type == pygame.JOYBUTTONDOWN:  # right joystick is moved out of resting position
+    
+        if event.type == pygame.JOYBUTTONDOWN:  # right joystick is moved out of resting position or select button is pressed
             print("pygame.JOYBUTTONDOWN event")
-            print("button:", event.button)
-            if event.button == 8: # select was pressed
-                writeToSerial(STOP + 'a')
-                writeToSerial(STOP + 'b')
-                writeToSerial(STOP + 'c')
-                writeToSerial(STOP + 'd')
-            if event.button == 6: # left front button pressed
-                pass
-            if event.button == 4: # left back button pressed
-                pass
-            if event.button == 5: #right front button pressed
-                pass
-            if event.button == 7: # right back button pressed
-                pass
-            if event.button == 0: # right joystick up
-                writeToSerial(FORWARD + 'b')
-                writeToSerial(FORWARD + 'c')
-            if event.button == 1: # right joystick right
-                writeToSerial(BACKWARD + 'd')
-                writeToSerial(FORWARD + 'a')
-            if event.button == 2: # right joystick down
-                writeToSerial(BACKWARD + 'b')
-                writeToSerial(BACKWARD + 'c')
-            if event.button == 3: # right joystick left
-                writeToSerial(BACKWARD + 'a')
-                writeToSerial(FORWARD + 'd')
+            print(event)
+            print("button:", event.button) 
+            if event.joy == 0 : 
+                if event.button == 8: # select was pressed
+                    writeToSerial(STOP + 'a')
+                    writeToSerial(STOP + 'b')
+                    writeToSerial(STOP + 'c')
+                    writeToSerial(STOP + 'd')
+
+
+                if event.button == 0: # right joystick up
+                    writeToSerial(FORWARD + 'b')
+                    writeToSerial(FORWARD + 'c')
+                if event.button == 1: # right joystick right
+                    writeToSerial(BACKWARD + 'd')
+                    writeToSerial(FORWARD + 'a')
+                if event.button == 2: # right joystick down
+                    writeToSerial(BACKWARD + 'b')
+                    writeToSerial(BACKWARD + 'c')
+                if event.button == 3: # right joystick left
+                    writeToSerial(BACKWARD + 'a')
+                    writeToSerial(FORWARD + 'd')
+            if event.joy == 1: 
+                if event.button == 0: # 1 button is presed on controller 1, 
+                    writeToSerial("5z") 
+                if event.button == 1: # 2 button is pressed on controller 1
+                    writeToSerial( "5y") 
+                if event.button == 2: # 3 button is pressed on controller 1
+                    writeToSerial("-5z") 
+                if event.button == 3: # 4 button is pressed on controller 1
+                    writeToSerial("-5y") 
         if event.type == pygame.JOYBUTTONUP:# right joystick has moved back to resting position
             print("pygame.JOYBUTTONDOWN event")
             print(event)
             print("button:", event.button)
-            if event.button == 8:
-                writeToSerial(STOP + 'a') 
-                writeToSerial(STOP + 'b')
-                writeToSerial(STOP + 'c')
-                writeToSerial(STOP + 'd')
-            if event.button == 0: # right joystick up
-                writeToSerial(STOP + 'b')
-                writeToSerial(STOP + 'c')
-            if event.button == 1: # right joystick right
-                writeToSerial(STOP + 'd')
-                writeToSerial(STOP + 'a')
-            if event.button == 2: # right joystick down
-                writeToSerial(STOP + 'b')
-                writeToSerial(STOP + 'c')
-            if event.button == 3: # right joystick left
-                writeToSerial(STOP + 'a')
-                writeToSerial(STOP + 'd')
+            if event.joy == 0:
+                if event.button == 8:
+                    writeToSerial(STOP + 'a')
+                    writeToSerial(STOP + 'b')
+                    writeToSerial(STOP + 'c')
+                    writeToSerial(STOP + 'd')
+                if event.button == 0: # right joystick up
+                    writeToSerial(STOP + 'b')
+                    writeToSerial(STOP + 'c')
+                if event.button == 1: # right joystick right
+                    writeToSerial(STOP + 'd')
+                    writeToSerial(STOP + 'a')
+                if event.button == 2: # right joystick down
+                    writeToSerial(STOP + 'b')
+                    writeToSerial(STOP + 'c')
+                if event.button == 3: # right joystick left
+                    writeToSerial(STOP + 'a')
+                    writeToSerial(STOP + 'd')
         elif event.type == pygame.JOYAXISMOTION:
             print("pygame.JOYAXISMOTION event")
             print(event)
-            axis = event.axis
-            value = round(event.value, 2)
-            print(axis, value)
-            if axis == 1 and value == 1: #moves back, left joystick down
-                writeToSerial(BACKWARD + 'a')
-                writeToSerial(BACKWARD + 'd')
-            if axis == 1 and value == -1: #moves forward, left joystick up
-                writeToSerial(FORWARD + 'a')
-                writeToSerial(FORWARD + 'd')
-            if axis == 0 and value == 1: #moves left, left joystick left
-                writeToSerial(FORWARD + 'b')
-                writeToSerial(BACKWARD + 'c')
-            if axis == 0 and value == -1:#moves right, left joystick right
-                writeToSerial(BACKWARD + 'b')
-                writeToSerial(FORWARD + 'c')
-            if value == 0 : #stops, left joystick neutral
-                writeToSerial(STOP + 'a')
-                writeToSerial(STOP + 'b')
-                writeToSerial(STOP + 'c')
-                writeToSerial(STOP + 'd')
-
-
+            if event.joy == 0: 
+                axis = event.axis
+                value = round(event.value, 2)
+                print(axis, value)
+                if axis == 1 and value == 1: #moves back, left joystick down
+                    writeToSerial(BACKWARD + 'a')
+                    writeToSerial(BACKWARD + 'd')
+                if axis == 1 and value == -1: #moves forward, left joystick up
+                    writeToSerial(FORWARD + 'a')
+                    writeToSerial(FORWARD + 'd')
+                if axis == 0 and value == 1: #moves left, left joystick left
+                    writeToSerial(FORWARD + 'b')
+                    writeToSerial(BACKWARD + 'c')
+                if axis == 0 and value == -1:#moves right, left joystick right
+                    writeToSerial(BACKWARD + 'b')
+                    writeToSerial(FORWARD + 'c')
+                if value == 0 : #stops, left joystick neutral
+                    writeToSerial(STOP + 'a')
+                    writeToSerial(STOP + 'b')
+                    writeToSerial(STOP + 'c')
+                    writeToSerial(STOP + 'd')
+        pygame.time.wait(15)
