b8eafe47deed3d911e96470945cfe073b7308559
Suhani0617 <95658128+Suhani0617@users.noreply.github.com>
2021-12-13 17:03:11 -0600

    Update controller_to_arduino.ino
    added commands to handle servo messages

--- controller_to_arduino.ino
+++ controller_to_arduino.ino
@@ -3,15 +3,28 @@
 #define THRUSTERS 4 // the number of Thrusters
 #define STOP 1500
 
+#define SERVOS 4 // the number of Servos
+
 int thrusterPins[THRUSTERS] = {2,3,4,5}; // Thrusters on pins 2 to 5
 Servo thrusters[THRUSTERS];
+
+int servoPins[SERVOS] = {6,7,8,9}; // Servos on pins 6 to 9 
+Servo servos[SERVOS];
+
 
 
 void setup() {
   Serial.begin(19200);
   for(int i=0; i < THRUSTERS; i++)
-  { thrusters[i].attach(thrusterPins[i]); 
-  thrusters[i].writeMicroseconds(STOP);
+  { 
+    thrusters[i].attach(thrusterPins[i]); 
+    thrusters[i].writeMicroseconds(STOP);
+  }
+
+    for(int i=0; i < SERVOS; i++)
+  { 
+    servos[i].attach(servoPins[i]); 
+    servos[i].write(0);
   }
   delay(7000);
   // put your setup code here, to run once:
@@ -30,6 +43,7 @@ void serviceSerial()
   { 
     int speed = Serial.parseInt();
     char ch = Serial.read();
+    
     if (ch >= 'a' && ch < 'a' + THRUSTERS)
      { 
         Serial.print("Thruster "); 
@@ -38,6 +52,14 @@ void serviceSerial()
         Serial.println(speed);
        thrusters[ch - 'a'].write(speed);
       }
+   if (ch >= 'w' && ch < 'w' + SERVOS) 
+   {
+        Serial.print("Servo "); 
+        Serial.print(ch - 'w' + 1);
+        Serial.print("   add to speed ");
+        Serial.println(speed);
+       //thrusters[ch - 'w'].write(speed);
+   }
+      
   }
 }
-
