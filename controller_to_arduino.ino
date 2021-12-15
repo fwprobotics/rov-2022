0dae1eed2374e9120025dfeba2e1518602a0046e
Suhani0617 <95658128+Suhani0617@users.noreply.github.com>
2021-12-15 16:40:11 -0600

    Update controller_to_arduino.ino
    finish code for the servos for pan and tilt

--- controller_to_arduino.ino
+++ controller_to_arduino.ino
@@ -11,6 +11,10 @@ Servo thrusters[THRUSTERS];
 int servoPins[SERVOS] = {6,7,8,9}; // Servos on pins 6 to 9 
 Servo servos[SERVOS];
 
+int servoPositions[SERVOS] = {10,10,10,10}; 
+
+
+
 
 
 void setup() {
@@ -24,7 +28,8 @@ void setup() {
     for(int i=0; i < SERVOS; i++)
   { 
     servos[i].attach(servoPins[i]); 
-    servos[i].write(0);
+    servos[i].write(servoPositions[i]);
+    
   }
   delay(7000);
   // put your setup code here, to run once:
@@ -58,7 +63,262 @@ void serviceSerial()
         Serial.print(ch - 'w' + 1);
         Serial.print("   add to speed ");
         Serial.println(speed);
-       //thrusters[ch - 'w'].write(speed);
+       int currentServoPosition = servoPositions[ch - 'w'];
+
+       if (speed >0 && currentServoPosition + speed <= 170) {
+        servoPositions[ch - 'w'] = speed + currentServoPosition;
+        servos[ch - 'w'].write(servoPositions[ch - 'w'] );   
+       }#include <Servo.h>
+
+#define THRUSTERS 4 // the number of Thrusters
+#define STOP 1500
+
+#define SERVOS 4 // the number of Servos
+
+int thrusterPins[THRUSTERS] = {2,3,4,5}; // Thrusters on pins 2 to 5
+Servo thrusters[THRUSTERS];
+
+int servoPins[SERVOS] = {6,7,8,9}; // Servos on pins 6 to 9 
+Servo servos[SERVOS];
+
+int servoPositions[SERVOS] = {10,10,10,10}; 
+
+
+
+
+
+void setup() {
+  Serial.begin(19200);
+  for(int i=0; i < THRUSTERS; i++)
+  { 
+    thrusters[i].attach(thrusterPins[i]); 
+    thrusters[i].writeMicroseconds(STOP);
+  }
+
+    for(int i=0; i < SERVOS; i++)
+  { 
+    servos[i].attach(servoPins[i]); 
+    servos[i].write(servoPositions[i]);
+    
+  }
+  delay(7000);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  serviceSerial();
+  delay(15);
+  // put your main code here, to run repeatedly:
+
+}
+void serviceSerial()
+{
+  if (Serial.available())
+  { 
+    int speed = Serial.parseInt();
+    char ch = Serial.read();
+    
+    if (ch >= 'a' && ch < 'a' + THRUSTERS)
+     { 
+        Serial.print("Thruster "); 
+        Serial.print(ch - 'a' + 1);
+        Serial.print("   set to ");
+        Serial.println(speed);
+       thrusters[ch - 'a'].write(speed);
+      }
+   if (ch >= 'w' && ch < 'w' + SERVOS) 
+   {
+        Serial.print("Servo "); 
+        Serial.print(ch - 'w' + 1);
+        Serial.print("   add to speed ");
+        Serial.println(speed);
+       int currentServoPosition = servoPositions[ch - 'w'];
+
+       if (speed >0 && currentServoPosition + speed <= 170) {
+        servoPositions[ch - 'w'] = speed + currentServoPosition;
+        servos[ch - 'w'].write(servoPositions[ch - 'w'] );   
+       }
+
+       if(speed<0 && currentServoPosition + speed >= 10) {
+         servoPositions[ch - 'w'] = speed + currentServoPosition;
+        servos[ch - 'w'].write(servoPositions[ch - 'w'] );  
+        
+        
+       }
+   }
+      
+  }
+}#include <Servo.h>
+
+#define THRUSTERS 4 // the number of Thrusters
+#define STOP 1500
+
+#define SERVOS 4 // the number of Servos
+
+int thrusterPins[THRUSTERS] = {2,3,4,5}; // Thrusters on pins 2 to 5
+Servo thrusters[THRUSTERS];
+
+int servoPins[SERVOS] = {6,7,8,9}; // Servos on pins 6 to 9 
+Servo servos[SERVOS];
+
+int servoPositions[SERVOS] = {10,10,10,10}; 
+
+
+
+
+
+void setup() {
+  Serial.begin(19200);
+  for(int i=0; i < THRUSTERS; i++)
+  { 
+    thrusters[i].attach(thrusterPins[i]); 
+    thrusters[i].writeMicroseconds(STOP);
+  }
+
+    for(int i=0; i < SERVOS; i++)
+  { 
+    servos[i].attach(servoPins[i]); 
+    servos[i].write(servoPositions[i]);
+    
+  }
+  delay(7000);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  serviceSerial();
+  delay(15);
+  // put your main code here, to run repeatedly:
+
+}
+void serviceSerial()
+{
+  if (Serial.available())
+  { 
+    int speed = Serial.parseInt();
+    char ch = Serial.read();
+    
+    if (ch >= 'a' && ch < 'a' + THRUSTERS)
+     { 
+        Serial.print("Thruster "); 
+        Serial.print(ch - 'a' + 1);
+        Serial.print("   set to ");
+        Serial.println(speed);
+       thrusters[ch - 'a'].write(speed);
+      }
+   if (ch >= 'w' && ch < 'w' + SERVOS) 
+   {
+        Serial.print("Servo "); 
+        Serial.print(ch - 'w' + 1);
+        Serial.print("   add to speed ");
+        Serial.println(speed);
+       int currentServoPosition = servoPositions[ch - 'w'];
+
+       if (speed >0 && currentServoPosition + speed <= 170) {
+        servoPositions[ch - 'w'] = speed + currentServoPosition;
+        servos[ch - 'w'].write(servoPositions[ch - 'w'] );   
+       }
+
+       if(speed<0 && currentServoPosition + speed >= 10) {
+         servoPositions[ch - 'w'] = speed + currentServoPosition;
+        servos[ch - 'w'].write(servoPositions[ch - 'w'] );  
+        
+        
+       }
+   }
+      
+  }
+}#include <Servo.h>
+
+#define THRUSTERS 4 // the number of Thrusters
+#define STOP 1500
+
+#define SERVOS 4 // the number of Servos
+
+int thrusterPins[THRUSTERS] = {2,3,4,5}; // Thrusters on pins 2 to 5
+Servo thrusters[THRUSTERS];
+
+int servoPins[SERVOS] = {6,7,8,9}; // Servos on pins 6 to 9 
+Servo servos[SERVOS];
+
+int servoPositions[SERVOS] = {10,10,10,10}; 
+
+
+
+
+
+void setup() {
+  Serial.begin(19200);
+  for(int i=0; i < THRUSTERS; i++)
+  { 
+    thrusters[i].attach(thrusterPins[i]); 
+    thrusters[i].writeMicroseconds(STOP);
+  }
+
+    for(int i=0; i < SERVOS; i++)
+  { 
+    servos[i].attach(servoPins[i]); 
+    servos[i].write(servoPositions[i]);
+    
+  }
+  delay(7000);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  serviceSerial();
+  delay(15);
+  // put your main code here, to run repeatedly:
+
+}
+void serviceSerial()
+{
+  if (Serial.available())
+  { 
+    int speed = Serial.parseInt();
+    char ch = Serial.read();
+    
+    if (ch >= 'a' && ch < 'a' + THRUSTERS)
+     { 
+        Serial.print("Thruster "); 
+        Serial.print(ch - 'a' + 1);
+        Serial.print("   set to ");
+        Serial.println(speed);
+       thrusters[ch - 'a'].write(speed);
+      }
+   if (ch >= 'w' && ch < 'w' + SERVOS) 
+   {
+        Serial.print("Servo "); 
+        Serial.print(ch - 'w' + 1);
+        Serial.print("   add to speed ");
+        Serial.println(speed);
+       int currentServoPosition = servoPositions[ch - 'w'];
+
+       if (speed >0 && currentServoPosition + speed <= 170) {
+        servoPositions[ch - 'w'] = speed + currentServoPosition;
+        servos[ch - 'w'].write(servoPositions[ch - 'w'] );   
+       }
+
+       if(speed<0 && currentServoPosition + speed >= 10) {
+         servoPositions[ch - 'w'] = speed + currentServoPosition;
+        servos[ch - 'w'].write(servoPositions[ch - 'w'] );  
+        
+        
+       }
+   }
+      
+  }
+}
+
+       if(speed<0 && currentServoPosition + speed >= 10) {
+         servoPositions[ch - 'w'] = speed + currentServoPosition;
+        servos[ch - 'w'].write(servoPositions[ch - 'w'] );  
+        
+        
+       }
    }
       
   }
