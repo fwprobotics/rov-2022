17cd3520c9291f0557fbf9cc8187c72e478c811c
bifft2 <bifft@fwparker.org>
2022-12-10 12:06:13 -0600

    Update analog_controller.py

--- analog_controller.py
+++ analog_controller.py
@@ -18,7 +18,7 @@ for port in serial.tools.list_ports.comports():
     #if "usb" in port.device.lower():
         correctdevice = port.device
 print("Using serial port: ", correctdevice)
-ser = serial.Serial(correctdevice, 9600, timeout=0.005)
+ser = serial.Serial(correctdevice, 19200, timeout=0.005)
 
 def writeToSerial(msg):
     print("writing", msg.encode())
@@ -46,8 +46,6 @@ controller.init()
 while True:
     for event in pygame.event.get():
         event_dict = event.dict
-        if event_dict.get("axis")==1:
-            print(event.dict)
         if event_dict.get("axis") == 4:
             degrees = translate (event.dict.get("value"), -1, 1, servo_b_close, servo_b_open)
             print(degrees)
@@ -56,9 +54,9 @@ while True:
             degrees = translate (event.dict.get("value"), -1, 1,servo_a_close, servo_a_open)
             print(degrees)
             writeToSerial(str(int(degrees)) + "x")
-
         if event_dict.get("axis") == 0:
             degrees = translate (event.dict.get("value"), -1, 1,1400, 1600)
+            degrees = round(degrees/10)*10
             print(degrees)
             if degrees > 1490 and degrees < 1510:
                 writeToSerial(str(int(1500)) + "a")
@@ -68,41 +66,46 @@ while True:
                 writeToSerial(str(int(degrees)) + "d")
         if event_dict.get("axis") == 1:
             c_degrees = translate (event.dict.get("value"), -1, 1,1400, 1600)
-            print(c_degrees)
+            c_degrees = round(c_degrees/10)*10
             if c_degrees > 1490 and c_degrees < 1510:
                 writeToSerial(str(int(1500)) + "c")
             else:
                 writeToSerial(str(int(c_degrees)) + "c")
             b_degrees = translate (event.dict.get("value"), -1, 1,1600, 1400)
+            b_degrees = round(b_degrees/10)*10
             if b_degrees > 1490 and b_degrees < 1510:
                 writeToSerial(str(int(1500)) + "b")
             else:
-                writeToSerial(str(int(c_degrees)) + "b")
-        pygame.time.wait(15)
-        if event_dict.get("axis")== 2 : 
+                writeToSerial(str(int(b_degrees)) + "b")
+        if event_dict.get("axis")== 2 :
             a_degrees = translate (event.dict.get("value"), -1, 1,1600, 1400)
+            a_degrees = round(a_degrees/10)*10
             if a_degrees > 1490 and a_degrees < 1510:
                 writeToSerial(str(int(1500)) + "a")
             else:
                 writeToSerial(str(int(a_degrees)) + "a")
             d_degrees = translate (event.dict.get("value"), -1, 1,1400, 1600)
+            d_degrees = round(d_degrees/10)*10
             if d_degrees > 1490 and d_degrees < 1510:
                 writeToSerial(str(int(1500)) + "d")
             else:
-                writeToSerial(str(int(a_degrees)) + "d")
-        if event_dict.get("axis")== 3 : 
+                writeToSerial(str(int(d_degrees)) + "d")
+        if event_dict.get("axis")== 3 :
             b_degrees = translate (event.dict.get("value"), -1, 1,1600, 1400)
+            b_degrees = round(b_degrees/10)*10
             if b_degrees > 1490 and b_degrees < 1510:
                 writeToSerial(str(int(1500)) + "b")
             else:
-                writeToSerial(str(int(a_degrees)) + "b")
+                writeToSerial(str(int(b_degrees)) + "b")
             c_degrees = translate (event.dict.get("value"), -1, 1,1400, 1600)
+            c_degrees = round(c_degrees/10)*10
             if c_degrees > 1490 and c_degrees < 1510:
                 writeToSerial(str(int(1500)) + "c")
             else:
-                writeToSerial(str(int(a_degrees)) + "c")   
+                writeToSerial(str(int(c_degrees)) + "c")
         
 
+        pygame.time.wait(15)
 
     out = ser.readline()
     if out:
