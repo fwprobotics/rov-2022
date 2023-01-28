612bd1d628d31e9bba7d18a71efef5b47260c937
spop8 <123829734+spop8@users.noreply.github.com>
2023-01-28 11:25:54 -0600

    add code to get rtc time in transmit

---
+++ transmitter_1:28:23.ino
@@ -0,0 +1,69 @@
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include "RTClib.h"
+// Include RadioHead Amplitude Shift Keying Library
+#include <RH_ASK.h>
+// Include dependant SPI Library 
+#include <SPI.h> 
+ 
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+#include <Wire.h>
+#include <Time.h>
+
+RTC_DS1307 rtc;
+
+
+void setup () {
+  rf_driver.init();
+  
+  Serial.begin(57600);
+
+#ifndef ESP8266
+  while (!Serial); // wait for serial port to connect. Needed for native USB
+#endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  // When time needs to be re-set on a previously configured device, the
+  // following line sets the RTC to the date & time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+}
+
+void loop () {
+    DateTime now = rtc.now();
+
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.println();
+    delay(1000);
+
+    
+    const char *msg = "Welcome to the Workshop!";
+    char UTC_time [9];
+    int num = snprintf(UTC_time, 9, "%02hhu:%02hhu:%02hhu" , now.hour(), now.minute(), now.second());
+    rf_driver.send((uint8_t *)UTC_time, strlen(UTC_time));
+    rf_driver.waitPacketSent();
+    delay(1000);
+}
