5329b2c6e53895393baec840887a70cf25efd34c
fmgb2025 <116223692+fmgb2025@users.noreply.github.com>
2023-11-08 16:44:23 -0600

    Update claw.ino

--- Test Code 2023/claw.ino
+++ Test Code 2023/claw.ino
@@ -1,10 +1,11 @@
 #include <Servo.h>
 int servoPin = 9;
+int servoPosition = 0;
+Servo myservo;
 
 void setup() { // put your setup code here, to run once:
   Serial.begin(19200);
   myservo.attach(9,1000,2000);
-  myservo.write(servoPosition(9));
   delay(1000);
   Serial.println("Start!");
 
@@ -13,5 +14,18 @@ void setup() { // put your setup code here, to run once:
 
 void loop() {
   // put your main code here, to run repeatedly:
+if(Serial.available()){
+  char ch = Serial.read();
+  if(ch=='w'){
+    myservo.write(180);
+  }else{
+    myservo.write(0);
+  }
+}
+
 
 }
+
+
+
+
