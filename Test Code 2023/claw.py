29911d59e52ec5cbdc1a1db8486bb22a9eb6cc56
fmgb2025 <116223692+fmgb2025@users.noreply.github.com>
2023-11-11 10:47:19 -0600

    Update claw.py

--- Test Code 2023/claw.py
+++ Test Code 2023/claw.py
@@ -3,9 +3,15 @@ import pygame
 import serial
 import serial.tools.list_ports
 
+pygame.joystick.init()
 print(pygame.joystick.get_count())
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 print(joysticks)
+pygame.init()
+
+def writeToSerial(msg):
+    print("writing", msg.encode())
+    ser.write(msg.encode())
 
 correctdevice = str()
 for port in serial.tools.list_ports.comports():
@@ -14,16 +20,19 @@ for port in serial.tools.list_ports.comports():
 print("Using serial port: ", correctdevice)
 ser = serial.Serial(correctdevice, 19200, timeout=0.005)
 
-if event.joy == 3:
-    writeToSerial("180z")
-if event.joy == 1:
-    writeToSerial("0z")
+while True:
 
-if event.joy == 11:
-    writeToSerial("180w")
-if event.joy == 13:
-    writeToSerial("0w")
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            if event.button == 3:
+                writeToSerial("180z")
+            if event.button == 1:
+                writeToSerial("0z")
 
+            if event.button == 11:
+                writeToSerial("180w")
+            if event.button == 13:
+                writeToSerial("0w")
 
 pygame.time.wait(15)
 out = ser.readline()
