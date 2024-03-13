2fafc9759b2ec0e78dec7f0b8328b615567197aa
Suhani0617 <95658128+Suhani0617@users.noreply.github.com>
2024-03-13 16:30:48 -0500

    Update stepper_motor.ino

--- Float/stepper_motor.ino
+++ Float/stepper_motor.ino
@@ -1,6 +1,7 @@
 #include "SparkFun_ProDriver_TC78H670FTG_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_ProDriver
 PRODRIVER myProDriver; //Create instance of this object
-
+int counter=0;
+int direction = 0;
 void setup() {
   Serial.begin(115200);
   Serial.println("SparkFun ProDriver TC78H670FTG Example 1");
@@ -8,8 +9,23 @@ void setup() {
 }
 
 void loop() {
-  myProDriver.step(250, 0); // 0 goes up/away from the tip
-  delay(1000);
-  myProDriver.step(250, 1); // 1 goes down/towards the tip
-  delay(1000);
+ if (counter >= 0 && counter <= 60) {
+      myProDriver.step(250,direction);
+      delay(1000);
+      counter = counter+1;
+      Serial.println(counter);
+ }
+ else {
+    counter = 0;
+    if(direction == 1){
+      direction = 0;
+    }else{
+      direction = 1;
+    }
+    delay(60000);
+
+ }
 }
+
+///1 means that the pump is at the top of the tip
+///0 means that the pump is at the bottem of the tip
