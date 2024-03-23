f77f55e9d152e361dc81289ebed194dbe21a7977
Suhani0617 <95658128+Suhani0617@users.noreply.github.com>
2024-03-23 11:13:53 -0500

    Update final_arduino.ino
    re-combined the claw and temp codes, no more counter, 1 servo works as of 3/23/24

--- Test Code 2023/final_arduino.ino
+++ Test Code 2023/final_arduino.ino
@@ -1,7 +1,6 @@
+#include <Servo.h>
 #include <Wire.h>
 #include "TSYS01.h"
-
-#include <Servo.h>
 int servoPin = 9;
 int servoPosition = 0;
 Servo myservo_1;
@@ -11,58 +10,54 @@ Servo mythruster_backward;
 Servo mythruster_up;
 Servo mythruster_down;
 
-int counter = 0;
-
 TSYS01 sensor;
 
-void setup() {
-    Serial.begin(19200);
-    Serial.println("Starting");
 
-    Wire.begin();
-  
-    while (!sensor.init()) {
-        Serial.println("TSYS01 device failed to initialize!");
-        delay(2000);
-    }
-    myservo_1.attach(9);
-    myservo_2.attach(10);
-    mythruster_up.attach(2);
-    mythruster_down.attach(3);
-    mythruster_forward.attach(4);
-    mythruster_backward.attach(5);
+void setup() { // put your setup code here, to run once:
+  Serial.begin(19200);
+  myservo_1.attach(9);
+  myservo_2.attach(10);
+
+  mythruster_up.attach(2);
+  mythruster_down.attach(3);
+  mythruster_forward.attach(4);
+  mythruster_backward.attach(5);
 
   delay(7000);
   Serial.println("Start!");
+
+  Wire.begin();
+  
+  while (!sensor.init()) {
+    Serial.println("TSYS01 device failed to initialize!");
+    delay(2000);
+ }
+
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+sensor.read();
+    Serial.print("Temperature: ");
+    Serial.print(sensor.temperature()); 
+    Serial.println(" deg C");
 
-    counter = counter + 1;
+if(Serial.available()){
 
-    if(counter% 100000000000000 == 0){
-      sensor.read();
-      Serial.print("Temperature: ");
-      Serial.print(sensor.temperature()); 
-      Serial.println(" deg C");
-    }
-
-    if(Serial.available()){
-
-    int speed = Serial.parseInt();
-    char ch = Serial.read();
-    if(ch=='y'){
-      myservo_1.write(speed);
-    } if(ch =='z'){
-      myservo_2.write(speed);
-    } if(ch=='a'){
-      mythruster_up.writeMicroseconds(speed);
-    } if(ch=='b'){
-      mythruster_down.writeMicroseconds(speed);
-    } if (ch=='c'){
-      mythruster_forward.writeMicroseconds(speed);
-    } if (ch=='d'){
-      mythruster_backward.writeMicroseconds(speed);
-    }
+  int speed = Serial.parseInt();
+  char ch = Serial.read();
+  if(ch=='y'){
+    myservo_1.write(speed);
+  } if(ch =='z'){
+    myservo_2.write(speed);
+  } if(ch=='a'){
+    mythruster_up.writeMicroseconds(speed);
+  } if(ch=='b'){
+    mythruster_down.writeMicroseconds(speed);
+  } if (ch=='c'){
+    mythruster_forward.writeMicroseconds(speed);
+  } if (ch=='d'){
+    mythruster_backward.writeMicroseconds(speed);
   }
+}
 }
