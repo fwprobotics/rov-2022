e580bd40d0f0759fbd43c633a695dd61759650f8
Suhani0617 <95658128+Suhani0617@users.noreply.github.com>
2024-04-01 16:43:35 -0500

    Update color.ino

--- Test Code 2023/color/color.ino
+++ Test Code 2023/color/color.ino
@@ -12,7 +12,6 @@ int blue = 2;
 int red = 3;
 int green = 4;
 int white = 5; 
-int yellow = 6;
 
 
 
@@ -33,7 +32,6 @@ void setup() { // put your setup code here, to run once:
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(white, OUTPUT);
-  pinMode(yellow, OUTPUT);
 
 
 }
