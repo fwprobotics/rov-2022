e34b9bdc1a6d33840f8a0b80113079e566975f83
fmgb2025 <116223692+fmgb2025@users.noreply.github.com>
2025-03-22 11:43:20 -0500

    Update individual_thruster_test.ino

--- 2023 Code/2023 (TEST) Code/indivisual_thruster_test.ino
+++ 2023 Code/2023 (TEST) Code/indivisual_thruster_test.ino
@@ -1,34 +1,15 @@
 #include <Servo.h>
-Servo myservo;
 Servo mythruster;
-int pls = 0;
-const int buttonPin1 = 3;
-const int buttonPin2 = 13;
-int buttonState = 0;
 void setup() {
-pinMode(buttonPin1, INPUT_PULLUP);
-pinMode(buttonPin2, INPUT_PULLUP);
-myservo.attach(9);
 Serial.begin (9600);
-mythruster.attach(10);
+mythruster.attach(4);
 
 }
 void loop() {
 // put your main code here, to run repeatedly:
-buttonState = digitalRead(buttonPin1);
-Serial.println(buttonState);
-if (buttonState == HIGH) {
   mythruster.writeMicroseconds(1500);
-} else  {
-    mythruster.writeMicroseconds(1300);
-}
-
-buttonState = digitalRead(buttonPin2);
-Serial.println(buttonState);
-if(buttonState ==HIGH){
-  myservo.write(90);
-}else{
-  myservo.write(0);
-}
+  delay(2000);
+  mythruster.writeMicroseconds(1300);
+  delay(2000);
 
 }
