3051e3b1a49c64f47941d1b884c253408c68e556
bifft2 <bifft@fwparker.org>
2025-04-19 11:48:56 -0500

    Update mostRecentFloat.ino

--- 2024 Code/2024 Float Code/mostRecentFloat.ino
+++ 2024 Code/2024 Float Code/mostRecentFloat.ino
@@ -1,26 +1,28 @@
 #include "SparkFun_ProDriver_TC78H670FTG_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_ProDriver
-PRODRIVER myProDriver; //Create instance of this object
-int counter = 0;
-int d = 0;
-int x = 3;
+#include <Wire.h>
+#include "MS5837.h"
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
 
-float pressuredata[11];
-float depthdata[11];
+PRODRIVER myProDriver; //Create instance of this object
+
+float pressuredata[48];
+float depthdata[48];
+int secdata[48];
 int whereArray = 0;
 float currentDepth = 0.0;
 float currentPressure = 0.0;
 long int t1;
 long int tnow;
+int secs;
+int counter = 0;
 
-#include <Wire.h>
-#include "MS5837.h"
-#include <RH_ASK.h>
-#include <SPI.h> // Not actually used but needed to compile
-char buff[10];
+char timeStamp[36];
+char pressureStr[7];
+char depthStr[7];
 
 MS5837 sensor;
 RH_ASK driver;
-
 
 void setup() {
 
@@ -49,10 +51,34 @@ void setup() {
   sensor.setModel(MS5837::MS5837_30BA);
   sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
 
-    for(int i = 0; i < 24; i++) {
-      myProDriver.step(250, d);
-      delay(500);
+  for(int i = 0; i < 24; i++) {
+    myProDriver.step(250, 0);
+    delay(500);
+    if(i%5 == 0) {
+      tnow = millis();
+      sensor.read();
+      currentDepth = sensor.depth();
+      currentPressure = sensor.pressure();
+      pressuredata[whereArray] = currentPressure;
+      depthdata[whereArray] = currentDepth;
+      secsdata[whereArray] = tnow;
+      whereArray++;
+
+      // remove this eventually
+      dtostrf(currentPressure, 2, 2, pressureStr);
+      Serial.println(pressureStr);
+
+      dtostrf(currentDepth, 2, 2, depthStr);
+      Serial.println(depthStr);
+
+      secs = (tnow/1000);
+      sprintf(timeStamp, "EX01, %02d sec, %7s kpa, %7s m", secs, pressureStr, depthStr);
+      Serial.println(timeStamp);
+
+      driver.send((uint8_t *)timeStamp, strlen(timeStamp));
+      driver.waitPacketSent(); 
     }
+  }
 
   t1 = millis();
 }
@@ -61,57 +87,63 @@ void loop() {
 
   tnow = millis();
 
-  myProDriver.step(125,d);
+  myProDriver.step(125, 0);
+  counter++;
   delay(1000);
-  counter = counter+1;
-  Serial.println(counter);
-  sensor.read();
-  // pressuredata[whereArray] = sensor.pressure();
 
-  currentDepth = sensor.depth();
-  currentPressure = sensor.pressure();
+  if(counter%3 == 0) {
+    sensor.read();
+    currentDepth = sensor.depth();
+    currentPressure = sensor.pressure();
+    pressuredata[whereArray] = currentPressure;
+    depthdata[whereArray] = currentDepth;
+    secsdata[whereArray] = tnow-t1;
+    whereArray++;
 
-  Serial.print("Depth: ");
-  Serial.print(currentDepth);
-  Serial.println(" m");
+    // remove this when we're done
+      dtostrf(currentPressure, 2, 2, pressureStr);
+      Serial.println(pressureStr);
+
+      dtostrf(currentDepth, 2, 2, depthStr);
+      Serial.println(depthStr);
+
+      secs = ((tnow-t1)/1000);
+      sprintf(timeStamp, "EX01, %02d sec, %7s kpa, %7s m", secs, pressureStr, depthStr);
+      Serial.println(timeStamp);
+
+      driver.send((uint8_t *)timeStamp, strlen(timeStamp));
+      driver.waitPacketSent(); 
+  }
 
 
-  Serial.print("Pressure: ");
-  Serial.print(currentPressure);
-  Serial.println(" mbar");
-  // depthdata[whereArray] = sensor.depth();
+  if(currentDepth <= -2 || millis() > 450000) {
+    for(int x = 0; x < 4; x++) {
+      delay(5000);
+      tnow = millis();
+      sensor.read();
+      currentDepth = sensor.depth();
+      currentPressure = sensor.pressure();
+      pressuredata[whereArray] = currentPressure;
+      depthdata[whereArray] = currentDepth;
+      secsdata[whereArray] = tnow-t1;
+      whereArray++;
 
+    }
+    tnow = millis();
 
-  char timeStamp[36];
-  char pressureStr[7];
-  char depthStr[7];
-  dtostrf(currentPressure, 2, 2, pressureStr);
-  Serial.println(pressureStr);
-
-  dtostrf(currentDepth, 2, 2, depthStr);
-  Serial.println(depthStr);
-
-    int secs = ((tnow-t1)/1000);
-    sprintf(timeStamp, "EX01, %02d sec, %7s kpa, %7s m", secs, pressureStr, depthStr);
-    Serial.println(timeStamp);
-
-    driver.send((uint8_t *)timeStamp, strlen(timeStamp));
-    driver.waitPacketSent(); 
-    delay(1000);
-
-  if(currentDepth <= -2.5) {
-    delay(20000); // wait 20 seconds
-
-      for(int j = 0; j < 20; j++) { // slowly let out water
+    for(int j = 0; j < 20; j++) { // slowly let out water
+      tnow = millis();
+      myProDriver.step(125, 1);
+      if(j%3 == 0) {
         sensor.read();
-        myProDriver.step(125, 1);
-
         currentDepth = sensor.depth();
         currentPressure = sensor.pressure();
+        pressuredata[whereArray] = currentPressure;
+        depthdata[whereArray] = currentDepth;
+        secsdata[whereArray] = tnow-t1;
+        whereArray++;
 
-        char timeStamp[36];
-        char pressureStr[7];
-        char depthStr[7];
+        // remove eventually
         dtostrf(currentPressure, 2, 2, pressureStr);
         Serial.println(pressureStr);
 
@@ -124,13 +156,96 @@ void loop() {
 
         driver.send((uint8_t *)timeStamp, strlen(timeStamp));
         driver.waitPacketSent(); 
+      }
+  }
 
-        delay(1000);
+  sensor.read();
+  currentDepth = sensor.depth();
+  currentPressure = sensor.pressure();
+  int i = 0;
+  while(currentDepth < 0) {
+    myProDriver.step(250, 1);
+    i++;
+    delay(500);
+    if(i%5 == 0) {
+      tnow = millis();
+      sensor.read();
+      currentDepth = sensor.depth();
+      currentPressure = sensor.pressure();
+      pressuredata[whereArray] = currentPressure;
+      depthdata[whereArray] = currentDepth;
+      whereArray++;
+
+      // remove this eventually
+      dtostrf(currentPressure, 2, 2, pressureStr);
+      Serial.println(pressureStr);
+
+      dtostrf(currentDepth, 2, 2, depthStr);
+      Serial.println(depthStr);
+
+      secs = (tnow/1000);
+      sprintf(timeStamp, "EX01, %02d sec, %7s kpa, %7s m", secs, pressureStr, depthStr);
+      Serial.println(timeStamp);
+
+      driver.send((uint8_t *)timeStamp, strlen(timeStamp));
+      driver.waitPacketSent(); 
     }
-    
+
+  }
+
+  // code to transmit all to the station
+  for (byte i = 0; i < 48; i = i + 1) {
+      currentDepth = depthdata[i];
+      currentPressure = pressuredata[i];
+      secs = secdata[i];
+
+      // remove this eventually
+      dtostrf(currentPressure, 2, 2, pressureStr);
+      Serial.println(pressureStr);
+
+      dtostrf(currentDepth, 2, 2, depthStr);
+      Serial.println(depthStr);
+
+      sprintf(timeStamp, "EX01, %02d sec, %7s kpa, %7s m", secs, pressureStr, depthStr);
+      Serial.println(timeStamp);
+
+      driver.send((uint8_t *)timeStamp, strlen(timeStamp));
+      driver.waitPacketSent(); 
+      delay(500);
+    } 
+
+    for(int i = 0; i < 24; i++) {
+      myProDriver.step(250, 0);
+      delay(500);
+      if(i%5 == 0) {
+        tnow = millis();
+        sensor.read();
+        currentDepth = sensor.depth();
+        currentPressure = sensor.pressure();
+        pressuredata[whereArray] = currentPressure;
+        depthdata[whereArray] = currentDepth;
+        secsdata[whereArray] = tnow;
+        whereArray++;
+
+        // remove this eventually
+        dtostrf(currentPressure, 2, 2, pressureStr);
+        Serial.println(pressureStr);
+
+        dtostrf(currentDepth, 2, 2, depthStr);
+        Serial.println(depthStr);
+
+        secs = (tnow/1000);
+        sprintf(timeStamp, "EX01, %02d sec, %7s kpa, %7s m", secs, pressureStr, depthStr);
+        Serial.println(timeStamp);
+
+        driver.send((uint8_t *)timeStamp, strlen(timeStamp));
+        driver.waitPacketSent(); 
+      }
+    }
+
+    t1 = millis();
   }
 }
-
 
 ///0 means that the pump is going towards the dry container
 ///1 means that the pump is going towards the tip of the syringe
